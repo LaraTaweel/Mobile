616f9c4362653ee6da7b3427c96cf5b24dca295f
LaraTaweel <s12115919@stu.najah.edu>
2026-03-05 21:35:45 +0200

    upload project from Snack

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+=======
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+>>>>>>> 7571d3ffff0e0eb0a4b2372a4d2dd667a85e1c79
 import { WebView } from "react-native-webview";
 
 export default function App() {
@@ -7,7 +12,11 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <WebView
         source={{ uri: "https://www.google.com" }}
+<<<<<<< HEAD
         style={styles.webview}
+=======
+        style={{ flex: 1 }}
+>>>>>>> 7571d3ffff0e0eb0a4b2372a4d2dd667a85e1c79
       />
     </SafeAreaView>
   );
@@ -17,7 +26,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+<<<<<<< HEAD
   webview: {
     flex: 1,
   },
 });
+=======
+});
+>>>>>>> 7571d3ffff0e0eb0a4b2372a4d2dd667a85e1c79
